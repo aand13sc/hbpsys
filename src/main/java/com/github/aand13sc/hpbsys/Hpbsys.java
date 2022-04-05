ae8d2cccbb9174d8c91eb591cd1d6a2a956f9dd7
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-04-05 16:32:35 +0900

    add: init project

---
+++ src/main/java/com/github/aand13sc/hpbsys/Hpbsys.java
@@ -0,0 +1,17 @@
+package com.github.aand13sc.hpbsys;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hpbsys extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
